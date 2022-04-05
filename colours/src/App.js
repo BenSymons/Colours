b282b49d59ca75d5b3cc0888a6b42c78da88baf8
ben <ben.s@builtbypixel.com>
2022-04-05 10:21:56 +0100

    change classname to title

--- colours/src/App.js
+++ colours/src/App.js
@@ -4,7 +4,7 @@ import ColoursList from './components/ColoursList';
 function App() {
   return (
     <div className="App">
-      <h1 className="heading">Today, the colour of the day is <span className="colour_of_day">PURPLE</span></h1>
+      <h1 className="title">Today, the colour of the day is <span className="colour_of_day">PURPLE</span></h1>
       <div className="color_container">
       <p>Other colours are:</p>
         <ColoursList/>
