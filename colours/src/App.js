95f3166de160c8dfc8eaaf01108b36e59c183c7c
Maria Olanipekun <m.olanipekun@hotmail.com>
2022-04-18 20:40:17 +0100

    added teal list item

--- colours/src/App.js
+++ colours/src/App.js
@@ -1,16 +1,20 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="heading">Today, the colour of the day is <span className="colour_of_day">Blue</span></h1>
+      <h1 className="heading">
+        Today, the colour of the day is
+        <span className="colour_of_day">Blue</span>
+      </h1>
       <div className="color_container">
-      <p>Other colours are:</p>
+        <p>Other colours are:</p>
         <ul>
           <li className="red">RED</li>
           <li className="blue">BLUE</li>
           <li className="yellow">YELLOW</li>
           <li className="lime">LIME</li>
+          <li className="teal">TEAL</li>
         </ul>
       </div>
     </div>
